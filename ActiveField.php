784035acef4af9c4db7db52727a198d194eb3a6f
fav <fav@equifax.ru>
2016-12-05 12:49:50 +0300

    Update ActiveField.php

--- ActiveField.php
+++ ActiveField.php
@@ -1,66 +1,77 @@
 <?php
 
-namespace fav/packages/activeField;
+namespace fav\activeField;
 
 use Yii;
-use yii/base/Html;
+use yii\base\Html;
+use yii\bootstrap\Widget;
 
 
-abstract class ActiveField extends \yii\bootstrap\Widget 
-	public $tag = 'div';
-	public $model;
-	public $attribute;
-	public $template = "{label}\n{wrapperBegin}\n{input}\n{hint}\n{error}\n{wrapperEnd}";
-	public $labelOptions = [];
-	public $inputOptions = [];
-	public $errorOptions = [];
-	public $hintOptions = [];
-	public $wrapOptions = [];
-	
-	protected function getLabel(){
-		return Html::activeLabel($this->model, $this->attribute, $this->labelOptions);
-	}
+abstract class ActiveField extends Widget
+{
 
-	abstract protected function getInput(){
-		return null;
-	}
-	protected function getHint(){
-		return Html::activeHint($this->model, $this->attribute, $this->hintOptions);
-	}
+    public $tag = 'div';
+    public $model;
+    public $attribute;
+    public $template = "{label}\n{wrapperBegin}\n{input}\n{hint}\n{error}\n{wrapperEnd}";
+    public $labelOptions = [];
+    public $inputOptions = [];
+    public $errorOptions = [];
+    public $hintOptions = [];
+    public $wrapOptions = [];
 
-	protected function getError(){
-		return Html::error($this->model, $this->attribute, $this->errorOptions);
-	}
+    protected function getLabel()
+    {
+        return Html::activeLabel($this->model, $this->attribute, $this->labelOptions);
+    }
 
-	protected function getWrapperBegin(){
-		return Html::beginTag($this->tag, $this->wrapOptions);
-	}
+    abstract protected function getInput();
 
-	protected function getWrapperend(){
-		return Html::endTag($this->tag);
-	}
+    protected function getHint()
+    {
+        return Html::activeHint($this->model, $this->attribute, $this->hintOptions);
+    }
 
-	public function init() {
-		parent::init();
-		$view = Yii::$app->getView();
-		$this->registerAssets();
-		$view->registerJs($this->getJs());
-	}
+    protected function getError()
+    {
+        return Html::error($this->model, $this->attribute, $this->errorOptions);
+    }
 
-	public function run() {
-		return str_replace(
-			['{label}','{input}','{hint}','{error}','{wrapperBegin}','{wrapperEnd}'],
-			[$this->getLabel(),$this->getInput(),$this->getHint(),$this->getError(),$this->getWrapperBegin(),$this->getWrapperend()],
-			$this->template
-		);
-	}
+    protected function getWrapperBegin()
+    {
+        return Html::beginTag($this->tag, $this->wrapOptions);
+    }
 
-	public function registerAssets() {
-		$view = $this->getView();
-		activeFieldAsset::register($view);
-	}
-	
-	private function getJs() {
-		return null;
-	}
+    protected function getWrapperend()
+    {
+        return Html::endTag($this->tag);
+    }
+
+    public function init()
+    {
+        parent::init();
+        $view = Yii::$app->getView();
+        $this->registerAssets();
+        $view->registerJs($this->getJs());
+    }
+
+    public function run()
+    {
+        return str_replace(
+            ['{label}', '{input}', '{hint}', '{error}', '{wrapperBegin}', '{wrapperEnd}'],
+            [$this->getLabel(), $this->getInput(), $this->getHint(), $this->getError(), $this->getWrapperBegin(), $this->getWrapperend()],
+            $this->template
+        );
+    }
+
+    public function registerAssets()
+    {
+        $view = $this->getView();
+        activeFieldAsset::register($view);
+    }
+
+    private function getJs()
+    {
+        return null;
+    }
 }
